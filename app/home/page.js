e012a84d3839aeba8bdcf757d2bd6e0067b84da8
lucas <lereveur731@gmail.com>
2024-09-23 18:43:42 +0200

    articles

--- app/home/page.js
+++ app/home/page.js
@@ -1,5 +1,20 @@
-import { redirect } from 'next/navigation';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 export default function Home() {
-  redirect('/');
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/');
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <link rel="canonical" href="https://www.eco-vrac.fr/" />
+      </Head>
+      <div>Redirection en cours...</div>
+    </>
+  );
 }
